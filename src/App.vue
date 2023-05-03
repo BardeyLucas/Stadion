d890c368a1eac930e03bc0065197d4d3b1b9a9e6
Barder_Lucas <bardey.estelle1@gmail.com>
2023-05-03 11:53:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,187 @@
+<script setup>
+//import footer from './components/footer.vue'
+//import header from './components/header.vue'
+</script>
+
+<template>
+    <body>
+    <h1>Carte des tournois près de chez vous</h1>
+
+    <div>
+        <h2>Jeux associés</h2>
+        <div><img>
+            <h5></h5>
+        </div>
+        <div><img>
+            <h5></h5>
+        </div>
+        <div><img>
+            <h5></h5>
+        </div>
+        <div><img>
+            <h5></h5>
+        </div>
+    </div>
+
+    <div>
+        <h2>Tournois disponible</h2>
+        <!--carte-->
+        <div><img>
+            <div>
+                <h6></h6>
+            </div>
+            <h4></h4>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+        </div>
+        <!--carte-->
+        <div><img>
+            <div>
+                <h6></h6>
+            </div>
+            <h4></h4>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+        </div>
+        <!--carte-->
+        <div><img>
+            <div>
+                <h6></h6>
+            </div>
+            <h4></h4>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+        </div>
+        <!--carte-->
+        <div><img>
+            <div>
+                <h6></h6>
+            </div>
+            <h4></h4>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+        </div>
+        <!--carte-->
+        <div><img>
+            <div>
+                <h6></h6>
+            </div>
+            <h4></h4>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+        </div>
+        <!--carte-->
+        <div><img>
+            <div>
+                <h6></h6>
+            </div>
+            <h4></h4>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+        </div>
+        <!--carte-->
+        <div><img>
+            <div>
+                <h6></h6>
+            </div>
+            <h4></h4>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+        </div>
+        <!--carte-->
+        <div><img>
+            <div>
+                <h6></h6>
+            </div>
+            <h4></h4>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+        </div>
+        <!--carte-->
+        <div><img>
+            <div>
+                <h6></h6>
+            </div>
+            <h4></h4>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+            <img>
+            <h6></h6>
+        </div>
+        <button>VOIR PLUS</button>
+    </div>
+    <div>
+        <h2>Classement</h2>
+        <button>Général</button>
+        <button>League of legends</button>
+        <button>sUPER sMASH brOS</button>
+        <button>Overwatch 2</button>
+        <button>sTREET FIGHTER</button>
+        <!--rajouter les carte-->
+        <button>VOIR PLUS</button>
+    </div>
+    <div>
+        <h2>Pourquoi acheter l’abonnement Premium ?</h2>
+        <h3>Avec l’abonnement Premium vous pouvez...</h3>
+        <div>
+            <h3>Vous mettre en avant pour être recruté
+                par des équipes Esport</h3>
+            <p>Vous cherché à vous lancer dans l’esport. Stadion est là pour vous aider.
+                Grâce à la mise en avant de votre profil personnalisé, nous vous aiderons à rentrer en contact avec des équipes professionnelles en temps que joueurs ou coach.</p>
+        </div>
+        <div>
+            <h3>Vous aider a recruter des joueurs ou des coachs</h3>
+            <p>À la recherche d’un nouveau membre dans votre équipe. Joueurs ou coachs, aucun problème avec notre moteur de recherche.
+                Celui-ci vous permettra de faire des recherches rapidement et détaillées afin de trouver la personne idéal pour compler ce vide.</p>
+        </div>
+        <div>
+            <h3>Vous permettre de mettre en avant vos tournois</h3>
+            <p>Vous recherchez à mettre plus en avant votre tournois. Stadion vous aidera à agrandir votre visibilité en mettant en avant votre tournois sur notre site.</p>
+        </div>
+        <button>Abonnement premium</button>
+    </div>
+    </body>
+</template>
