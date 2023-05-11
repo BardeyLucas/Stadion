4797f826484fffbfc94324b59a215d8e7ac29c56
Barder_Lucas <bardey.estelle1@gmail.com>
2023-05-11 19:01:43 +0200

    Intégration du contenu des pages utilitaire

---
+++ src/fr/Inscription.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img>
+        <form>
+            <input type="text">
+            <input type="date">
+            <input type="text">
+            <input type="text">
+            <input type="email">
+            <input type="tel">
+            <input type="password">
+        </form>
+        <button>S'INSCRIRE'</button>
+        <p>En cliquant sur “s’inscrire gratuitement”, vous acceptez</p>
+        <p>les conditions d’utilisation et la politique de</p>
+        <p>confidentialité de Stadion</p>
+        <h3>Vous avez déjà un compte ?</h3>
+        <h3>Connexion</h3>
+    </div>
+</template>
