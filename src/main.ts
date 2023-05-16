3705449362d321a1d61f3b57544940de41ce7baa
Barder_Lucas <bardey.estelle1@gmail.com>
2023-05-16 13:26:43 +0200

    16/05/2023

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import routes from '~/pages'
+
+import {createRouter, createWebHistory} from 'vue-router'
+
+import './assets/css/style.css'
+
+const app = createApp(App)
+
+app.use(
+    createRouter({
+        history: createWebHistory(),
+        routes
+    })
+)
+
+app.mount('#app')
