43140e6573c68202a00545173595326d4c101e5e
Barder_Lucas <bardey.estelle1@gmail.com>
2023-06-06 18:54:41 +0200

    Card fixe

--- ProjetS2/src/components/Class-Card-Profil.vue
+++ ProjetS2/src/components/Class-Card-Profil.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="col-span-12 flex h-12 text-White items-center border-b-2 border-DarkGray">
         <p class="ml-9 mr-3.5">1</p>
-        <img class="h-10 w-10 mr-2.5"/>
+        <img class="h-10 w-10 mr-2.5" src="../../public/img/Icones-perso/joueurs/Yasuo.png"/>
         <p class="ml-2.5">Yasuoplayer78</p>
         <p class="ml-auto w-20 text-center">Challenger</p>
         <p class="w-20 text-center">1 690 LP</p>
         <p class="w-20 text-center">967</p>
-        <img class="h-10 w-10 mr-2.5 rounded-full"/>
-        <img class="h-10 w-10 mr-2.5 rounded-full"/>
-        <img class="h-10 w-10 mr-2.5 rounded-full"/>
-        <img class="h-10 w-10 mr-24 rounded-full"/>
+        <img class="h-10 w-10 mr-2.5 rounded-full" src="../../public/img/Icones-jeux/Perso-1.webp"/>
+        <img class="h-10 w-10 mr-2.5 rounded-full" src="../../public/img/Icones-jeux/Perso-2.webp"/>
+        <img class="h-10 w-10 mr-2.5 rounded-full" src="../../public/img/Icones-jeux/Perso-3.webp"/>
+        <img class="h-10 w-10 mr-24 rounded-full" src="../../public/img/Icones-jeux/Perso-4.webp"/>
     </div>
 </template>
--- ProjetS2/src/components/Class-Card-Tournois.vue
+++ ProjetS2/src/components/Class-Card-Tournois.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="col-span-12 border-b-2 border-DarkGray flex gap-10 h-36 mt-12">
-        <img class="w-64 rounded-md"/>
+        <img class="w-64 rounded-md" src="../../public/img/Icones-jeux/Smash.webp"/>
         <div class="items-center py-8 gap-y-2">
             <time class="text-White">10 juin 2023</time>
-            <p class="text-Gold">Lol Championship</p>
-            <p class="text-White">League of legends . Unranked - Challenger . 5V5 . Montbéliard . 200€</p>
+            <p class="text-Gold">Battle Royale Championship</p>
+            <p class="text-White">Super Smash Bros Ultimate . Unranked . 1V1 . Montbéliard . 200€</p>
         </div>
     </div>
 </template>
