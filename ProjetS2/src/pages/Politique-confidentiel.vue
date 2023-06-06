59e3da46cd0129dbb0a7776c76b7d648b50f5652
Barder_Lucas <bardey.estelle1@gmail.com>
2023-06-06 07:44:52 +0200

    confidentialité

--- ProjetS2/src/pages/Politique-confidentiel.vue
+++ ProjetS2/src/pages/Politique-confidentiel.vue
@@ -1,3 +1,236 @@
+<script setup lang="ts">
+import Hero from '@/components/Hero.vue';
+</script>
 <template>
-
-</template>
+    <Hero />
+    <div class="grille text-White">
+        <h2 class="col-span-12 mt-24">1. Informations générales</h2>
+        <p class="col-span-11 col-start-2">Ce document (" Politique de confidentialité ") décrit comment Stadion, peut
+            collecter, utiliser et traiter vos données à caractère personnel. Stadion est le contrôleur des données
+            concernant le traitement de vos données personnelles tel que décrit dans la présente politique de
+            confidentialité.</p>
+        <p class="col-span-11 col-start-2">Cette politique de confidentialité s'applique lorsque vous utilisez la
+            Plateforme. Veuillez noter qu'en utilisant la Plateforme, vous pouvez être dirigé vers des sites Internet ou des
+            services fournis par des tiers où vos données personnelles sont traitées par ces tiers et régies par leurs
+            politiques de confidentialité respectives.</p>
+        <p class="col-span-11 col-start-2 ">Toutes les définitions de la présente Politique de confidentialité doivent être
+            interprétées conformément aux lois applicables en matière de protection des données qui font référence au
+            Règlement général sur la protection des données (Règlement n° 2016/679) et à la Directive sur la vie privée et
+            les communications électroniques (Directive 2002/58/CE), ainsi qu'aux mises en œuvre nationales et à la
+            législation nationale y afférente. Les définitions spécifiées dans les conditions d'utilisation s'appliquent
+            également à la présente politique de confidentialité.</p>
+        <h2 class="col-span-12 mt-12">2. Quelles sont les données personnelles traitées par Stadion ?</h2>
+        <p class="col-span-11 col-start-2">Stadion traite vos données personnelles à différentes fins. Vous trouverez
+            ci-dessous les données personnelles que nous traitons, l'objectif du traitement de vos données personnelles et
+            notre base juridique pour le traitement de vos données personnelles.</p>
+        <div class="col-start-2 col-span-3">
+            <p class="mb-5">Objet du traitement</p>
+            <p>Pour administrer votre compte utilisateur</p>
+        </div>
+        <div class="col-start-5 col-span-5">
+            <p class="mb-5">Données personnelles vous concernant</p>
+            <ul class="list-disc list-inside">
+                <li class="mb-2">Adresse e-mail</li>
+                <li class="mb-2">Mot de passe</li>
+                <li class="mb-2">Photo de profil</li>
+                <li class="mb-2">Nom d'utilisateur</li>
+                <li class="mb-2">Prénom et nom de famille</li>
+                <li class="mb-2">Adresse</li>
+                <li class="mb-2">Numéro de téléphone</li>
+                <li class="mb-2">Informations de paiement</li>
+                <li class="mb-2">Date de naissance </li>
+                <li class="mb-2">Pays</li>
+            </ul>
+        </div>
+        <div class="col-start-10 col-span-3">
+            <p class="mb-5">Base légale de Stadion pour le traitement</p>
+            <p>Le traitement est nécessaire pour que nous puissions respecter nos obligations contractuelles envers vous,
+                telles que la création et la gestion de votre compte utilisateur, ainsi que votre participation aux
+                compétitions.</p>
+        </div>
+        <div class="col-start-2 col-span-3">
+            <p>Pour vous proposer de participer à des compétitions et activités pertinentes pour votre lieu de résidence et
+                votre âge</p>
+        </div>
+        <div class="col-start-5 col-span-5">
+            <ul class="list-disc list-inside">
+                <li class="mb-2">Pays</li>
+                <li class="mb-2">Date de naissance</li>
+                <li class="mb-2">Adresse</li>
+                <li class="mb-2">Adresse IP</li>
+            </ul>
+        </div>
+        <div class="col-start-10 col-span-3">
+            <p>Il est dans notre intérêt légitime de traiter vos données personnelles afin de vous offrir la meilleure
+                expérience utilisateur possible. Il est nécessaire de collecter vos données personnelles pour pouvoir vous
+                présenter des activités pertinentes.</p>
+        </div>
+        <div class="col-start-2 col-span-3">
+            <p>Pour vérifier votre identité en jeu et vérifier les résultats des matchs lors des compétitions, ainsi que
+                pour recueillir des statistiques de match et de jeu</p>
+        </div>
+        <div class="col-start-5 col-span-5">
+            <ul class="list-disc list-inside">
+                <li>Compte de jeu (par exemple, compte Riot Games)</li>
+            </ul>
+        </div>
+        <div class="col-start-10 col-span-3">
+            <p>Le traitement est nécessaire pour que nous puissions respecter notre obligation contractuelle envers vous.
+            </p>
+        </div>
+        <div class="col-start-2 col-span-3">
+            <p>Pour gérer les questions de support et les réclamations</p>
+        </div>
+        <div class="col-start-5 col-span-5">
+            <ul class="list-disc list-inside">
+                <li class="mb-2">Adresse e-mail</li>
+                <li class="mb-2">Prénom et nom de famille</li>
+                <li class="mb-2">Nom d'utilisateur</li>
+            </ul>
+        </div>
+        <div class="col-start-10 col-span-3">
+            <p class="mb-2">Il est dans notre intérêt légitime de traiter vos données personnelles afin de pouvoir vous
+                aider avec vos questions de support. Il est nécessaire de collecter vos données personnelles pour pouvoir
+                vous assister dans vos demandes.</p>
+            <p>Si votre question concerne une réclamation, nous la traiterons sur la base légale de l'exécution de notre
+                obligation contractuelle.</p>
+        </div>
+        <div class="col-start-2 col-span-3">
+            <p>Pour vous envoyer des documents de marketing direct sur nos nouvelles fonctionnalités et jeux</p>
+        </div>
+        <div class="col-start-5 col-span-5">
+            <ul class="list-disc list-inside">
+            <li class="mb-2">Adresse e-mail</li>
+            <li class="mb-2">Prénom et nom de famille</li>
+            <li class="mb-2">Nom d'utilisateur</li>
+        </ul>
+    </div>
+    <div class="col-start-10 col-span-3">
+        <p>Données de jeu et statistiquesIl est dans notre intérêt légitime de traiter vos données personnelles dans le
+            but de vous envoyer des documents de marketing direct. Vous avez un droit absolu de vous opposer au
+            traitement de vos données personnelles à des fins de marketing direct.</p>
+    </div>
+    <div class="col-start-2 col-span-3">
+        <p>Pour gérer votre demande de désinscription de nos publicités</p>
+    </div>
+    <div class="col-start-5 col-span-5">
+        <ul class="list-disc list-inside">
+            <li class="mb-2">Adresse e-mail</li>
+            <li class="mb-2">Données de jeu et statistiques</li>
+            <li class="mb-2">Records de tournois </li>
+            <li class="mb-2">Toute autre information concernant votre activité de jeu</li>
+        </ul>
+    </div>
+    <div class="col-start-10 col-span-3">
+        <p>Il est dans notre intérêt légitime de traiter vos données personnelles pour nous permettre de mettre en avant
+            les participants sur la plateforme et de promouvoir notre plateforme.</p>
+    </div>
+    <div class="col-start-2 col-span-3">
+        <p>Afficher et recommander du contenu, des compétitions et des informations pertinentes sur la plateforme.</p>
+    </div>
+    <div class="col-start-5 col-span-5">
+        <ul class="list-disc ml-4">
+            <li class="mb-2">Informations techniques, telles que votre adresse IP, les informations de votre navigateur,
+                la langue préférée, ainsi que le système d'exploitation que vous utilisez.</li>
+            <li class="mb-2">Compétitions et activités auxquelles vous avez participé ou que vous avez organisées.</li>
+        </ul>
+    </div>
+    <div class="col-start-10 col-span-3">
+        <p>Il est dans notre intérêt légitime de traiter vos données personnelles dans le but de vous recommander des
+            compétitions et du contenu pertinent sur la plateforme.</p>
+    </div>
+    <h2 class="mt-12 mb-6 col-span-12">3. Divulgation de données à caractère personnel</h2>
+    <p class="mb-2 col-span-11 col-start-2">Vos données personnelles peuvent être divulguées aux Organisateurs, sponsors
+        ou partenaires lorsque vous participez à des activités ou à des concours organisés par eux.</p>
+    <p class="mb-2 col-span-11 col-start-2">Nous pouvons également divulguer vos données personnelles aux prestataires
+        de services que nous utilisons pour fournir la Plateforme. Les fournisseurs de services appartiennent aux
+        catégories suivantes : (i) solutions de paiement, telles que PayPal et (ii) fournisseurs de services
+        informatiques.</p>
+    <p class="mb-2 col-span-11 col-start-2">Stadion ne divulguera ni ne partagera intentionnellement aucune donnée
+        personnelle vous concernant avec des tiers sans votre consentement, sauf si Stadion, en toute bonne foi, estime
+        qu'une telle divulgation est nécessaire pour se conformer à la loi applicable ou aux dispositions contractuelles
+        de nos Conditions d'utilisation.</p>
+    <p class="mb-2 col-span-11 col-start-2">Stasion s'efforce toujours de traiter vos données personnelles au sein de
+        l'UE/EEE. Cependant, dans certaines situations, vos données personnelles peuvent être transférées et traitées
+        dans un pays situé en dehors de l'UE/EEE. Dans ces cas, Stasion s'assure que des mesures appropriées sont en
+        place pour protéger vos données personnelles conformément à la législation applicable en matière de protection
+        des données.</p>
+    <p class="col-span-11 col-start-2">Stadion se réserve le droit de collecter et de divulguer des informations
+        démographiques générales et d'autres informations commerciales qui ne vous identifient pas personnellement, à
+        toute personne, sans votre consentement supplémentaire.</p>
+    <h2 class="mt-12 mb-6 col-span-12">4. Cookies</h2>
+    <p class="mb-2 col-span-11 col-start-2">Un " cookie " est un petit fichier texte envoyé par le serveur de Stadion à
+        votre navigateur. Le cookie ne peut pas vous identifier personnellement et peut uniquement reconnaître le
+        navigateur installé sur votre ordinateur et utilisé pour accéder à la Plateforme. Différents cookies sont
+        envoyés à différents ordinateurs si vous accédez à la Plateforme à partir de différents ordinateurs. Les cookies
+        sont nécessaires pour que la Plateforme fonctionne correctement et en toute sécurité, par exemple lorsque vous
+        vous connectez à votre compte utilisateur. Les cookies sont également utilisés pour mesurer le trafic de données
+        sur la plate-forme et pour des raisons statistiques.</p>
+    <p class="mb-2 col-span-11 col-start-2">Il existe deux types de cookies : les "cookies de session" et les "cookies
+        permanents". La plate-forme utilise à la fois des cookies de session et des cookies permanents.</p>
+    <p class="mb-2 col-span-11 col-start-2">Un cookie de session est envoyé entre votre ordinateur et le serveur de
+        Stadion afin que la Plateforme fonctionne correctement pendant votre visite. Un cookie de session disparaît
+        lorsque le navigateur est fermé et n'est donc pas stocké sur l'ordinateur.</p>
+    <p class="mb-2 col-span-11 col-start-2">Un cookie permanent est stocké dans votre ordinateur et permet à la
+        plate-forme de reconnaître l'adresse IP et le navigateur web de votre ordinateur lors de votre prochaine
+        connexion.</p>
+    <p class="col-span-11 col-start-2">Vous pouvez refuser que nous utilisions des cookies sur votre ordinateur en
+        modifiant les paramètres de votre navigateur. De tels paramètres peuvent impliquer que certaines fonctions de la
+        Plateforme ne fonctionneront pas correctement ou ne seront pas disponibles.</p>
+    <h2 class="mt-12 mb-6 col-span-12">5. Stockage des données personnelles</h2>
+    <p class="col-span-11 col-start-2">Vos données personnelles seront conservées aussi longtemps que vous aurez un
+        compte d'utilisateur actif. Onze mois après votre dernière connexion, vous recevrez un rappel concernant votre
+        inactivité. En cas de nouvelle inactivité, votre compte d'utilisateur sera bloqué. Deux ans après votre dernière
+        connexion, votre compte utilisateur sera supprimé pour cause d'inactivité, sauf si les données sont nécessaires
+        pour une raison spécifique ou si Stadion a convenu avec vous de conserver les données personnelles pendant une
+        période déterminée. Stadion se réserve le droit de supprimer les données personnelles plus tôt si Stadion le
+        juge approprié.</p>
+    <h2 class="mt-12 mb-6 col-span-12">6. Vos droits</h2>
+    <p class="mb-2 col-span-11 col-start-2">Vous trouverez ci-dessous les droits dont vous disposez en vertu de la
+        législation applicable en matière de protection des données. Si vous souhaitez envoyer une demande concernant
+        l'un de vos droits, veuillez nous contacter à l'adresse électronique dans la page contact.</p>
+    <p class="mb-2 col-span-11 col-start-2">Afin de répondre à votre demande, celle-ci doit être aussi précise que
+        possible. Veuillez utiliser des indicateurs tels que : le type de données, le type de document, la fonction/le
+        service concerné et/ou les personnes de contact concernées, etc. Votre demande ne pourra pas être traitée si
+        elle n'est pas spécifique ou claire.</p>
+    <p class="mb-2 col-span-11 col-start-2">Droit d'accès - Vous avez le droit de demander l'accès et des informations
+        complémentaires concernant le traitement de vos données à caractère personnel. Vous avez le droit d'obtenir
+        gratuitement une copie des données à caractère personnel que nous traitons à votre sujet une (1) fois par année
+        civile. Pour toute copie supplémentaire demandée par vous, nous pouvons facturer des frais raisonnables basés
+        sur les coûts administratifs.</p>
+    <p class="mb-2 col-span-11 col-start-2">Droit de rectification - Si les données personnelles que Stadion détient à
+        votre sujet sont inexactes ou incomplètes, vous avez le droit de nous demander de les rectifier ou de les
+        compléter. Si ces données personnelles ont été transmises à un tiers avec votre consentement ou pour des raisons
+        juridiques, nous lui demanderons également de les rectifier ou de les compléter.</p>
+    <p class="mb-2 col-span-11 col-start-2">Droit à l'effacement ou à la limitation - Vous avez le droit d'effacer ou de
+        limiter le traitement de vos données personnelles.</p>
+    <p class="mb-2 col-span-11 col-start-2">Droit d'opposition - Vous avez le droit de vous opposer au traitement de vos
+        données personnelles par Stadion sur la base de nos intérêts légitimes.</p>
+    <p class="mb-2 col-span-11 col-start-2">Droit de retrait - Si vous nous avez donné votre consentement au traitement
+        de vos données personnelles mais que vous changez d'avis, vous avez le droit de retirer votre consentement à
+        tout moment, et Stadion doit alors cesser le traitement de vos données personnelles sur la base de votre
+        consentement. Le retrait de votre consentement n'affecte pas la légalité du traitement basé sur le consentement
+        avant son retrait.</p>
+    <p class="col-span-11 col-start-2">Portabilité des données - Si le traitement est fondé sur des motifs légaux, le
+        consentement ou l'exécution d'un contrat, vous avez le droit à la portabilité des données. La portabilité des
+        données signifie que vous pouvez recevoir les données à caractère personnel que vous nous avez fournies, dans un
+        format structuré, couramment utilisé et lisible par machine, et que vous avez le droit de transférer ces données
+        à caractère personnel à un tiers.</p>
+    <h2 class="mt-12 mb-6 col-span-12">7. Mineurs</h2>
+    <p class="col-span-11 col-start-2">La Plateforme ne s'adresse pas aux personnes âgées de moins de treize (13) ans.
+        Si vous êtes le parent ou le tuteur d'une personne âgée de moins de 13 ans et que vous apprenez que l'enfant
+        nous a fourni des données à caractère personnel sans votre consentement, veuillez nous contacter à l'adresse
+        électronique dans la page contact.</p>
+    <h2 class="mt-12 mb-6 col-span-12">8. Réclamation</h2>
+    <p class="col-span-11 col-start-2">Si vous avez des plaintes concernant notre traitement de vos données
+        personnelles, vous pouvez déposer une plainte auprès de l'Autorité française de protection des données (la CNIL)
+        si vous estimez que Stadion n'a pas répondu de manière adéquate à votre demande.</p>
+    <h2 class="mt-12 mb-6 col-span-12">9. Avis de modification de la politique de confidentialité</h2>
+    <p class="col-span-11 col-start-2">La présente politique de confidentialité peut être occasionnellement mise à jour,
+        c'est pourquoi vous devez la lire régulièrement. Toute modification de la présente Politique de confidentialité
+        sera publiée sur la Plateforme et, le cas échéant, vous sera notifiée par écrit. Les modifications importantes
+        apportées à la présente politique de confidentialité vous seront toujours envoyées par courrier électronique.
+    </p>
+    <h2 class="mt-12 mb-6 col-span-12">10. Nous contacter</h2>
+    <p class="col-span-11 col-start-2 text-Gold mb-24">Page contact</p>
+</div></template>
